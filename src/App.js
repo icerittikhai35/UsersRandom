ca037472a9999832979ae25d00b57a8e7177414b
icerittikhai35 <ritthikai.khan@gmai.com>
2023-01-06 22:59:48 +0700

    Edit API ,Use Rapid API free

--- src/App.js
+++ src/App.js
@@ -1,34 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
 
 
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   axios.get('https://randomuser.me/api/?results=100')
+  //     .then(res => setData(res.data.results));
+  // }, [])
+
+  // console.log(data);
+  const [rice, setRice] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const options = {
+    method: 'GET',
+    url: 'https://edamam-food-and-grocery-database.p.rapidapi.com/parser',
+    params: { ingr: 'apple' },
+    headers: {
+      'X-RapidAPI-Key': '9eaa2a4320msheb344641ce08f70p1142b1jsnd7be46274f54',
+      'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
+    }
+  };
+
+  // axios.request(options).then(response => {
+  //   setRice(response.data.hints);
+  // }).catch(function (error) {
+  //   console.error(error);
+  // });
 
 
   useEffect(() => {
-    axios.get('https://randomuser.me/api/?results=100')
-      .then(res => setData(res.data.results));
-  }, [])
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        axios.request(options).then(response => {
+          setRice(response.data.hints);
+        })
 
-  console.log(data);
+      } catch (error) {
+        console.error(error.message);
+      }
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
-      {data.map((val, idx) => (
+     
+
+      {rice.map((val, idx) => (
         <div key={idx} className="info">
-          <img src={val.picture.large} />
-          <h5 className='h5'>{val.name.title}. {val.name.first} {val.name.last}</h5>
-          <h5 className='h5'>Country {val.location.country}</h5>
+          <h5 className='h5'>{val.food.label}</h5>
+          <h5 className='h5'>{val.food.nutrients.ENERC_KCAL} Cal</h5>
           <img
-            src={'https://countryflagsapi.com/png/' + val.location.country}
-            style={{width:'10rem', height:'6rem'}}
+            src={val.food.image}
+            style={{ width: '70%', }}
+            alt={val.food.label}
           />
         </div>
       ))}
+
+
     </div>
   );
 }
